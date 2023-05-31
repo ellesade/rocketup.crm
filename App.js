11d410d4dcd9224be7e61fbe26e5d40810f2abd3
ellesade <135165941+ellesade@users.noreply.github.com>
2023-05-31 23:33:52 +0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,9 @@
+const btn1 = document.getElementById('button1')
+const btn2 = document.getElementById('button2')
+btn1.addEventListener('click', function(){
+    alert('Скоро с вами свяжутся! =)')
+})
+btn2.addEventListener('click', function(){
+    alert('Заполните форму для заявки!')
+})
+
---
+++ usluga.js
@@ -0,0 +1,6 @@
+const btn3 = document.getElementById('button3')
+btn3.addEventListener('click', function(){
+    prompt('Введите ФИО')
+    prompt('Введите номер телефона!')
+    alert('Скоро с вами свяжутся! =)')
+})
